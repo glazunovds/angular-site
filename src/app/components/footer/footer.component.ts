72b148ca25e447a522c37dc801e16ded26245121
glazunov_ds <glazunovzayac@gmail.com>
2019-11-25 00:42:09 +0200

    fixed parallax popup

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material';
 import {MasterDialogComponent} from '@appComponents/master-dialog/master-dialog.component';
+declare const $: any;
 
 @Component({
   selector: 'app-footer',
@@ -20,5 +21,9 @@ export class FooterComponent implements OnInit {
       width: '800px',
       data: {}
     });
+  }
+
+  scrollTop(): void {
+    $('html, body').stop().animate({scrollTop:0}, 500, 'swing');
   }
 }
